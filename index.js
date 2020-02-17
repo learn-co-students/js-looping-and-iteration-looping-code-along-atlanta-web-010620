27a6714b22c03e60b9d54b48f6ded3dd337d8a40
Jacy Leech <jacy.atlanta@gmail.com>
2020-02-17 15:08:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(gifts, event) {
+    let temp = [];
+    for (let i = 0; i < gifts.length; i++) {
+        temp.push(`Thank you, ${gifts[i]}, for the wonderful ${event} gift!`)
+    }
+    return temp;
+}
+
+function countDown() {
+    let countdown = 10;
+    while (countdown > -1) {
+        console.log(countdown--);
+    }
+}
